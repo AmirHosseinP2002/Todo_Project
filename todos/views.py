c6bc0d8261558165b32f1796c2e0c52072bb6744
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-07-27 22:02:41 +0330

    add todos query and send data to todos_list.html

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 
 def todos_list_view(request):
-    return render(request, 'todos/todos_list.html', {})
+    todos = Todo.objects.filter(author=request.user).order_by('-datetime_created')
+    return render(request, 'todos/todos_list.html', {
+        'todos': todos
+    })
