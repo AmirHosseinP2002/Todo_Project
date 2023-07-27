78aac02ecd3c1fb84053418a266c0e2313bcad8a
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-07-27 21:35:01 +0330

    Add Todo model in todos app

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'datetime_created', 'datetime_updated', 'is_completed']
+    list_editable = ['author']
+    raw_id_fields = ['author']
+    list_filter = ['author', 'is_completed']
+
+
+admin.site.register(Todo, TodoAdmin)
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
