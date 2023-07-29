81d5cd78407fc0eb294880804022071411193d2f
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-07-29 17:45:45 +0330

    add login and logout system

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'todos_list'
+LOGOUT_REDIRECT_URL = 'home'
